1ffbcd59b46933fa83860df130b0b796dc778ffc
Ricardo <ricardo.rjdc@gmail.com>
2025-03-27 13:45:44 +0100

    New Entry readjustments

--- Manager/Manager/Views/PasswordPageWindows/NewEntryWindow.xaml.cs
+++ Manager/Manager/Views/PasswordPageWindows/NewEntryWindow.xaml.cs
@@ -40,6 +40,24 @@ namespace Manager.Views.PasswordPageWindows
             this._id = id;
         }
 
-      
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            VisiblePasswordBox.Text = PasswordBox.Password;
+            VisiblePasswordBox.Visibility = Visibility.Visible;
+            PasswordBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = VisiblePasswordBox.Text;
+            PasswordBox.Visibility = Visibility.Visible;
+            VisiblePasswordBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void CloseNewEntry(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
     }
 }
